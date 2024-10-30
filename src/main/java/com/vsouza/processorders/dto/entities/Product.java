7eb2ee15d8957990be4b21b4850746b951ca267e
veronica.souza <veronica.souza@icts.com.br>
2024-10-30 19:45:53 -0300

    ler arquivo e salvar no banco

--- src/main/java/com/vsouza/processorders/dto/entities/Product.java
+++ src/main/java/com/vsouza/processorders/dto/entities/Product.java
@@ -1,12 +1,29 @@
 package com.vsouza.processorders.dto.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
-@Builder
+@Builder @AllArgsConstructor @NoArgsConstructor
+@Entity
+@Table(name = "products")
 public class Product {
 
-    private Long productId;
-    private Double value;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal productPrice;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderProduct> orderProducts = new ArrayList<>();
+
 }
--- src/main/java/com/vsouza/processorders/dto/entities/User.java
+++ src/main/java/com/vsouza/processorders/dto/entities/User.java
@@ -1,12 +1,26 @@
 package com.vsouza.processorders.dto.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @Builder
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
-    private Long userId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Order> orders;
 }
